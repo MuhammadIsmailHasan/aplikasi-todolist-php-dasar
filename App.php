4ebc9404c5fd6d2feec18c5ccd59086c07b079a2
MuhammadIsmailHasan <muhisan10@gmail.com>
2023-05-13 14:23:22 +0700

    save

--- App.php
+++ App.php
@@ -9,5 +9,5 @@ require_once __DIR__ . "/View/ViewShowTodoList.php";
 require_once __DIR__ . "/View/ViewAddTodoList.php";
 require_once __DIR__ . "/View/ViewRemoveTodoList.php";
 
-"APLIKASI TODOLIST" . PHP_EOL;
+echo "APLIKASI TODOLIST" . PHP_EOL;
 ViewShowTodoList();
